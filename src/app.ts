ac403a57357dc05be8c4006d5da64be9eb20e0a2
fork-yoshino <yoshino@fork.co.jp>
2022-11-08 19:46:10 +0900

    Fix 不要な記述を削除

--- src/app.ts
+++ src/app.ts
@@ -380,6 +380,6 @@ class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
   }
 }
 
-const prjInput = new ProjectInput();
-const activePrjList = new ProjectList('active');
-const finishedPrjList = new ProjectList('finished');
+new ProjectInput();
+new ProjectList('active');
+new ProjectList('finished');
